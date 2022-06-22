052df3c7747467cfe6241c5887001c30a0ba717f
Daniel Giordano <danielgiordano@Daniels-MacBook-Pro.local>
2022-06-21 22:53:18 -0400

    built scaffolding

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -6,7 +6,7 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: 'Appltiools Documentation',
+  title: 'Appltiools Docs',
   tagline: 'Get started visual testing today ',
   url: 'https://your-docusaurus-test-site.com',
   baseUrl: '/',
