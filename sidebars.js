1d5ffd8e82dc58322114aa24366a31b1c12dcc14
Daniel Giordano <danielgiordano@Daniels-MacBook-Pro.local>
2022-08-03 09:53:29 -0400

    sidebar is almost done

--- sidebars.js
+++ sidebars.js
@@ -12,7 +12,6 @@
 /** @type {import('@docusaurus/plugin-content-docs').SidebarsConfig} */
 const sidebars = {
   tutorialSidebar: [
-    
     {
       type: 'category',
       label: 'Getting Started',
@@ -31,7 +30,6 @@ const sidebars = {
         'getting-started/retrieving-api-key',
         'getting-started/analyzing-differences',
         'getting-started/match-level',
-        'getting-started/accessibility',
         'getting-started/root-cause-analysis',
       ],
     },
@@ -43,34 +41,84 @@ const sidebars = {
         {
           type: 'category',
           label: 'Web SDKs',
-          items: ['quickstart/web/cypress'],
+          items: [
+            'quickstart/web/cypress',
+            {
+              type: 'category',
+              label: 'Selenium',
+              items: ['quickstart/web/selenium/java', 'quickstart/web/selenium/javascript', 'quickstart/web/selenium/python','quickstart/web/selenium/c-sharp', 'quickstart/web/selenium/ruby', 'quickstart/web/selenium/php']
+            },
+            {
+              type: 'category',
+              label: 'WebdriverIO',
+              items: ['quickstart/web/webdriver/webdriverio-4','quickstart/web/webdriver/webdriverio-5','quickstart/web/webdriver/webdriverio-6',]
+            },
+            'quickstart/web/puppeteer',
+            'quickstart/web/playwright',
+            'quickstart/web/protractor',
+            'quickstart/web/testcafe',
+            'quickstart/web/watir',
+            'quickstart/web/capybara',
+            {
+              type: 'category',
+              label: 'LeanFT',
+              items: ['quickstart/web/leanft/leanft-c', 'quickstart/web/leanft/leanft-javascript',]
+            },
+          ],
         },
         {
           type: 'category',
           label: 'Mobile SDKs',
-          items: ['quickstart/mobile/espresso'],
+          items: [
+            {
+              type: 'category',
+              label: 'Appium',
+              items: ['quickstart/mobile/appium/appium-native-csharp','quickstart/mobile/appium/appium-native-java', 'quickstart/mobile/appium/appium-native-javascript','quickstart/mobile/appium/appium-native-python','quickstart/mobile/appium/appium-native-ruby','quickstart/mobile/appium/appium-native-php',]
+            },
+            'quickstart/mobile/espresso',
+            {
+              type: 'category',
+              label: 'XCUI',
+              items: ['quickstart/web/leanft/leanft-c', 'quickstart/web/leanft/leanft-javascript',]
+            },
+            {
+              type: 'category',
+              label: 'Calabash',
+              items: ['quickstart/web/leanft/leanft-c', 'quickstart/web/leanft/leanft-javascript',]
+            },
+          ],
         },
         {
           type: 'category',
           label: 'Desktop SDKs',
-          items: ['quickstart/desktop/windows-uft'],
+          items: ['quickstart/desktop/windows-uft', 'quickstart/desktop/windows-coded-UI',],
         },
         {
           type: 'category',
           label: 'Codeless',
-          items: ['quickstart/codeless/selenium-ide'],
+          items: ['quickstart/codeless/selenium-ide', 'quickstart/codeless/pdfs', 'quickstart/codeless/uft'],
         },
         {
           type: 'category',
           label: 'Screenshots',
-          items: ['quickstart/screenshots/cli'],
+          items: ['quickstart/screenshots/cli', 'quickstart/screenshots/c-sharp', 'quickstart/screenshots/java', 'quickstart/screenshots/javascript', 'quickstart/screenshots/python', 'quickstart/screenshots/ruby', 'quickstart/screenshots/php', 'quickstart/screenshots/xctest-swift', 'quickstart/screenshots/xctest-objective-c'],
         },
+      ],
+    },
+    
+    {
+      type: 'category',
+      label: 'References',
+      collapsed: true,
+      items: [
+        'reference/sdk-api/index',
+        'reference/server-api/index',
       ],
     },
     {
       type: 'category',
       label: 'Guides',
-      collapsed: false,
+      collapsed: true,
       items: [
         {
           type: 'category',
@@ -81,11 +129,12 @@ const sidebars = {
     },
     {
       type: 'category',
-      label: 'References',
-      collapsed: false,
+      label: 'Concepts',
+      collapsed: true,
       items: [
-        'reference/sdk-api/index',
-        'reference/server-api/index',
+        'concepts/what-is-visual-ai',
+        'concepts/comparing-applitools',
+        'getting-started/accessibility',
       ],
     },
   ],
